42dbb08254a85a07ca61b51198134c226093b650
Krankenkopf <petricowork@gmail.com>
2021-11-29 04:42:41 +0300

    /error handler - cases typed

--- src/s3-utils/apiError.ts
+++ src/s3-utils/apiError.ts
@@ -1,3 +1,4 @@
+import { StatusCode } from './statusCodes';
 export default class ApiError extends Error {
     status: number
 
@@ -9,22 +10,22 @@ export default class ApiError extends Error {
     }
 
     static badRequest(message: string) {
-        return new ApiError(400, message)
+        return new ApiError(StatusCode.BAD_REQUEST, message)
     }
 
     static unauthorized(message: string) {
-        return new ApiError(401, message)
+        return new ApiError(StatusCode.UNAUTHORIZED, message)
     }
 
     static accessForbidden(message: string) {
-        return new ApiError(403, message)
+        return new ApiError(StatusCode.FORBIDDEN, message)
     }
 
     static notFound(message: string) {
-        return new ApiError(404, message)
+        return new ApiError(StatusCode.NOT_FOUND, message)
     }
 
     static serverError(message: string) {
-        return new ApiError(500, message)
+        return new ApiError(StatusCode.INTERNAL_SERVER_ERROR, message)
     }
 }
--- src/s4-middleware/errorHanderMdw.ts
+++ src/s4-middleware/errorHanderMdw.ts
@@ -1,13 +1,13 @@
 import {NextFunction, Request, Response} from "express";
 import ApiError from "../s3-utils/apiError";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (error: Error, req: Request, res: Response, next: NextFunction) => {
     console.log("Error handling middleware called");
     console.log("Path:", req.path);
-    console.error("Error occured:", err);
-    if (err instanceof ApiError) {
+    console.error("Error occured:", error);
+    if (error instanceof ApiError) {
         console.log("Error is known");
-        return res.status(err.status).json({message: err.message})
+        return res.status(error.status).json({ data: {},auth: {}, info: [error.message]})
     }
-    return res.status(500).json({message: "unpredicted server error"})
+    return res.status(500).json({ data: {}, auth: {}, info: ["unpredicted server error"]})
 }
