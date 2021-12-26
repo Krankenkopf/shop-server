f70a02e6694c4885838159d35cf33a90dff9b90d
Krankenkopf <petricowork@gmail.com>
2021-12-26 08:45:21 +0300

    /cookies - cookie cleaner added

--- src/s1-main/cookie.ts
+++ src/s1-main/cookie.ts
@@ -2,10 +2,13 @@ import { Response } from "express";
 
 export const cookieSettings = {}
 //export const cookieSettings = DEV_VERSION ? {} : {sameSite: "none" as const, secure: true};
-export const resCookie = (res: Response) => {
+export const resAddCookie = (res: Response) => {
     const expiresIn = new Date(2022, 1, 1)
     return res.cookie("unauthId", res.locals.unauthId, {
         ...cookieSettings,
         expires: expiresIn,
     });
+}
+export const resClearCookie = (res: Response, key: string) => {
+    return res.clearCookie(key);
 }
