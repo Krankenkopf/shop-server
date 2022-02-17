52ff5a38a6b1699671753597142052fdea5d0c8d
Krankenkopf <petricowork@gmail.com>
2022-02-17 10:13:16 +0300

    /cookies - expiresIn changed

--- src/s1-main/cookie.ts
+++ src/s1-main/cookie.ts
@@ -3,7 +3,7 @@ import { Response } from "express";
 export const cookieSettings = {}
 //export const cookieSettings = DEV_VERSION ? {} : {sameSite: "none" as const, secure: true};
 export const resAddCookie = (res: Response) => {
-    const expiresIn = new Date(2022, 1, 1)
+    const expiresIn = new Date(Date.now()+1000*60*60*24*7)
     return res.cookie("unauthId", res.locals.unauthId, {
         ...cookieSettings,
         expires: expiresIn,
